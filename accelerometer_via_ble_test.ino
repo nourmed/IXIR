e70791c5b3b8cb82bf201ab1ebd77674f4b427d5
unknown <nour_med01@hotmail.fr>
2019-06-10 18:40:39 +0100

    first test of accelerometer_ble

---
+++ accelerometer_via_ble_test.ino
@@ -0,0 +1,76 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+#include <SparkFunLIS3DH.h>
+#include "Wire.h"
+#include "SPI.h"
+
+LIS3DH myIMU( I2C_MODE, 0x18 );
+float x=0 , y=0 ,z=0;
+bool _BLEClientConnected = false;
+#define accelerometerService BLEUUID(0CEAE362-FB5D-49B8-B0DB-288D9273DD12);
+BLECharacteristic XaxisChar(BLEUUID(),BLECharacteristic::PROPERTY_NOTIFY);
+BLECharacteristic YaxisChar(BLEUUID(),BLECharacteristic::PROPERTY_NOTIFY);
+BLECharacteristic ZaxisChar(BLEUUID(),BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor AccelerometerDescriptor(BLEUUID((uint16_t)0x2901));
+
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      _BLEClientConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      _BLEClientConnected = false;
+    }
+};
+
+void InitBLE() {
+  BLEDevice::init("IXIR");
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+
+  BLEService *pACC = pServer->createService(accelerometerService);
+  pACC->addCharacteristic(&XaxisChar);
+  pACC->addCharacteristic(&YaxisChar);
+  pACC->addCharacteristic(&ZaxisChar);
+  AccelerometerDescriptor.setValue("Accelerometer value");
+  XaxisChar.addDescriptor(&AccelerometerDescriptor);
+  XaxisChar.addDescriptor(new BLE2902());
+  YaxisChar.addDescriptor(&AccelerometerDescriptor);
+  YaxisChar.addDescriptor(new BLE2902());
+  ZaxisChar.addDescriptor(&AccelerometerDescriptor);
+  ZaxisChar.addDescriptor(new BLE2902());
+
+
+  pServer->getAdvertising()->addServiceUUID(accelerometerService);
+  pACC->start();
+  // Start advertising
+  pServer->getAdvertising()->start();
+}
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Start");
+  InitBLE();
+  myIMU.begin();
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+x=myIMU.readFloatAccelX();
+y=myIMU.readFloatAccelY();
+z=myIMU.readFloatAccelZ();
+
+ //update values and notify client of XaxisChar
+  XaxisChar.setValue(x);
+  XaxisChar.notify();
+//update values and notify client of YaxisChar
+  YaxisChar.setValue(y);
+  YaxisChar.notify();
+//update values and notify client of ZaxisChar
+  ZaxisChar.setValue(z);
+  ZaxisChar.notify();
+  delay(1000);
+}
