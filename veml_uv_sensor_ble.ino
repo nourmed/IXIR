eec308ed4dc35642f5581c73c98c1ca8ee36b772
unknown <nour_med01@hotmail.fr>
2019-06-14 12:34:31 +0100

    BLE test

--- veml_uv_sensor_ble.ino
+++ veml_uv_sensor_ble.ino
@@ -1,4 +1,8 @@
 #include <Wire.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
 
 
 
@@ -7,8 +11,10 @@
 
 
 
+bool _BLEClientConnected = false;
+BLEDescriptor UVDescriptor(BLEUUID((uint16_t)0x2901));
 
-
+BLECharacteristic UVCharacteristic(BLEUUID((uint16_t)0x2A76), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
 #define I2C_ADDR 0x38 //0x38 and 0x39
 
 //Integration Time
@@ -16,7 +22,38 @@
 #define IT_1   0x1 //1T
 #define IT_2   0x2 //2T
 #define IT_4   0x3 //4T
+//BLE callback function
+class MyServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    _BLEClientConnected = true;
+  };
 
+  void onDisconnect(BLEServer* pServer) {
+    _BLEClientConnected = false;
+  }
+};
+//initiate BLE Server
+void InitBLE() {
+  BLEDevice::init("IXIR");
+
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(BLEUUID((uint16_t)0x181A));
+
+  UVDescriptor.setValue("UV index value");
+  UVCharacteristic.addDescriptor(&UVDescriptor);
+  // Create a BLE Descriptor
+  HCharacteristic.addDescriptor(new BLE2902());
+  TCharacteristic.addDescriptor(new BLE2902());
+
+  pService->addCharacteristic(&UVCharacteristic);
+  pService->start();
+  // Start advertising
+  pServer->getAdvertising()->start();
+}
 void setup()
 {
   Serial.begin(9600);
@@ -28,6 +65,7 @@ void setup()
   Wire.write((IT_1<<2) | 0x02);
   Wire.endTransmission();
   delay(500);
+  InitBLE();
 }
 
 void loop()
@@ -47,6 +85,9 @@ void loop()
 
   uv = (msb<<8) | lsb;
   Serial.println(uv, DEC); //output in steps (16bit)
-
+  uint8_t uvindex=random(1,9);
+  Serial.printnln(uvindex);
+  UVCharacteristic.setValue(uvindex,2);
+  UVCharacteristic.notify();
   delay(1000);
 }
